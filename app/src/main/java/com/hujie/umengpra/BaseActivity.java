8f53661eba998cf6ab8a92918f8894b2d1c0de60
hujie <380211629@qq.com>
2017-01-10 18:39:53 +0800

    Umeng statistic Practice

---
+++ app/src/main/java/com/hujie/umengpra/BaseActivity.java
@@ -0,0 +1,25 @@
+package com.hujie.umengpra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.umeng.analytics.MobclickAgent;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+}
--- app/src/main/java/com/hujie/umengpra/MainActivity.java
+++ app/src/main/java/com/hujie/umengpra/MainActivity.java
@@ -1,13 +1,32 @@
 package com.hujie.umengpra;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.umeng.analytics.MobclickAgent;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        MobclickAgent.UMAnalyticsConfig config=
+//                new MobclickAgent.UMAnalyticsConfig
+//                        (this,"58747f4c07fe653817000c95","channel");
+//        MobclickAgent.startWithConfigure(config);
+    }
+
+    long curent=0;
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis()-curent>2000){
+            Toast.makeText(this, "再按一下退出", Toast.LENGTH_LONG).show();
+            curent=System.currentTimeMillis();
+        }else {
+            super.onBackPressed();
+            MobclickAgent.onKillProcess(this);
+            System.exit(0);
+        }
     }
 }
